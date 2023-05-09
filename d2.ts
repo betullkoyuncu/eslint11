6a2a1328a1813d39b7fdbe0181b60bac3aef4d8e
betullkoyuncu <117205036+betullkoyuncu@users.noreply.github.com>
2023-05-09 13:06:59 +0300

    Update d2.ts

--- d2.ts
+++ d2.ts
@@ -4,3 +4,4 @@ export { default as testItemActivityStatus } from "./testItemActivityStatus";
 export { default as contentSharing } from "./contentSharing";
 export { default as userContext } from "./userContext";
 export { default as collectionNames } from "./itemBank";
+import typoFile from "./typo-file"; 
